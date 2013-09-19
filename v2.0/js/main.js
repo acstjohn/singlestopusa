3b0f2877769fefe64c623e9825b0c101f06af24a
acstjohn <acstjohn@gmail.com>
2013-09-18 23:23:54 -0400

    added jquery soap

--- v2.0/js/main.js
+++ v2.0/js/main.js
@@ -37,3 +37,39 @@ function CreateEntity() {
    /* ok(guidExpr.test(contactId), "Creating a contact should returned the new record's ID in GUID format. ");     */
 
 }
+
+function CreateContactASJ() {
+    $.soap({
+        url: 'https://singlestopusa.api.crm.dynamics.com/XRMServices/2011/Organization.svc/',
+        method: 'create',
+
+        params: {
+            BusinessEntity: 'contact',
+            firstname: 'John',
+            lastname: 'Smith'
+        },
+
+        request: function (SOAPRequest)  {
+            alert(SOAPRequest.toString());
+            var temp = "<soap:Header><CrmAuthenticationToken" // xmlns='http://schemas.microsoft.com/crm/2007/WebServices'><AuthenticationType xmlns='http://schemas.microsoft.com/crm/2007/CoreTypes'>0</AuthenticationType><OrganizationName xmlns='http://schemas.microsoft.com/crm/2007/CoreTypes'>singlestopusa</OrganizationName><CallerId xmlns='http://schemas.microsoft.com/crm/2007/CoreTypes'>00000000-0000-0000-0000-000000000000</CallerId></CrmAuthenticationToken></soap:Header>"" + SOAPRequest;
+            SOAPRequest = temp;
+            alert(SOAPRequest.toString());
+
+        },
+
+        success: function (soapResponse) {
+            // do stuff with soapResponse
+            // if you want to have the response as JSON use soapResponse.toJSON();
+            // or soapResponse.toString() to get XML string
+            // or soapResponse.toXML() to get XML DOM
+               alert(soapResponse.toString());
+
+
+        },
+        error: function (SOAPResponse) {
+            // show error
+            alert(SOAPResponse.toString());
+        }
+    });
+
+}
